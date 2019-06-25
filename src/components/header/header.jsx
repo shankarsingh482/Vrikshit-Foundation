ed850fe01197349a52cb76f1709f66b2f9a4c88b
BA29575 <shankar.singh@idc.bedbath.com>
2019-06-25 14:13:09 +0530

    nav-top-updated

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,31 +1,62 @@
 import React from 'react';
 import {
-  NavLink,
+  Route,
   BrowserRouter ,
 } from 'react-router-dom';
-import styles from './header.css';
-import classnames from 'classnames'
-
+import './header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faFacebook, faTwitter , faInstagram} from '@fortawesome/free-brands-svg-icons'
+import logo from '../../assests/logo/homePageicon.jpg'
 class Header extends React.PureComponent{
 
 render() {
 
+const NavBar = [
+  {id:1,navBar:'Home',path:'/',component:'Home'},
+  {id:2,navBar:'About Us',path:'/aboutus',component:'AboutUs'},
+  {id:3,navBar:'Contact Us',path:'contactus',component:'ContactUs'},
+  {id:4,navBar:'Gallery',path:'gallery',component:'Gallery'},
+]
+
+const TopHeaderLink = [
+  {id:1,TopHeaderLink:'#',TopHeaderLinkName:'FAQ'},
+  {id:2,TopHeaderLink:'#',TopHeaderLinkName:'Forum'},
+  {id:3,TopHeaderLink:'#',TopHeaderLinkName:'Contact'}
+]
+
+const SocialMedia = [
+  {id:1,link:"",IclassName:faFacebook},
+  {id:2,link:"",IclassName:faTwitter},
+  {id:3,link:"",IclassName:faInstagram},
+]
+
   return(
-    <BrowserRouter >
-<header>
-    <div clas="conatiner">
-      <div class="nav-header">
-      <a href="#" class="js-fh5co-nav-toggle fh5co-nav-toggle dark"><i></i></a>
-					<h1 id="fh5co-logo"><a href="index.html">Vrikshit Foundation</a></h1>
-      </div>
-    </div>
-    <ul className={classnames(styles.header)}>
-            <li><NavLink to="/">Vrikshit Foundation</NavLink></li>
-            <li><NavLink to="/aboutus">About Us</NavLink></li>
-            <li><NavLink to="/contact">Contact Us</NavLink></li>
-            <li><NavLink to="/gallery">Gallery</NavLink></li>
-          </ul>
-</header>
+    <BrowserRouter>
+    	<div className="container nav_above_design">
+				<div className="row">
+					<div className="col-md-6 col-sm-6">
+						{TopHeaderLink.map(p => <a href={p.TopHeaderLink} className="nav_above_text_position">{p.TopHeaderLinkName}</a>)}
+					</div>
+					<div className="col-md-6 col-sm-6 text-right social social_icon_style">
+          {SocialMedia.map(p => <a href={p.link} className="social_icon_position"><FontAwesomeIcon icon ={p.IclassName}/></a>)}
+					</div>
+				</div>
+			</div>
+   <div className="container">
+      <div className="row">
+           <div className="col-md-6 col-sm-6">
+             <a class="navbar-brand" href="#">
+            <img className="logo_design" src ={logo} alt="Vrikshit Foundation"/>
+            </a>
+            </div>
+            <div className="col-md-6 col-sm-6">
+                 <ul className="header">
+                     {NavBar.map(p => <li key={p.id}><Route path={p.path} component={p.component} >{p.navBar}</Route></li>)}
+                </ul>
+            </div>
+     </div>
+  </div>
+
 </BrowserRouter >
   )
 }
