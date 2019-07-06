0fda827e7895b59edf3763f43c55b519d69f620a
Avantika Bharti <BA29605@idc.bedbath.com>
2019-07-06 20:54:33 +0530

    initial commit for footer

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import Header from '../src/components/header/header'
 import TopBanner from '../src//components/banner/topBanner'
 import Feature from '../src/components/features/features'
 import MainGallery from '../src/components/mainGallery/mainGallery'
+import Footer from './components/footer/footer';
 class App extends React.Component{
   render(){
     return (
-    <>
-    <Header/>
-    <TopBanner/>
-    <Feature/>
-    <MainGallery/>
-    </>
+      <>
+        <Header/>
+        <TopBanner/>
+        <Feature/>
+        <MainGallery/>
+        <Footer />
+      </>
     )
   }
 }
-
 
 export default App;
