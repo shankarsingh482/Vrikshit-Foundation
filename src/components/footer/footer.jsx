5e4042737661d0cd3e2268f594265002e6217fab
Avantika Bharti <BA29605@idc.bedbath.com>
2019-07-06 20:52:36 +0530

    initial commit for footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="footer-text">&copy;2019 Vrikshit Foundation</p>
+    </div>
+  )
+};
+
+export default Footer;
