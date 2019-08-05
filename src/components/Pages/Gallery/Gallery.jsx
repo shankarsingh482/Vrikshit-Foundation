99e40db69c9afc6d43c7a348f3b1f435d0d08f5b
Avantika Bharti <BA29605@idc.bedbath.com>
2019-08-05 22:53:02 +0530

    Gallery Page initial layout

--- src/components/Pages/Gallery/Gallery.jsx
+++ src/components/Pages/Gallery/Gallery.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import './Gallery.css';
+import MainGalleryListmap from '../../mainGallery/mainGalleryListmap';
+import { GalleryData } from './GalleryData';
+import HorizontalLine from '../../common/horizontalline';
 
 class Gallery extends React.PureComponent {
   render() {
     return (
-      <div>
-        <h1 className="text-center">Gallery</h1>
+      <div class="galleryContainer">
+        <div className="galleryBanner">
+          <h1 className="galleryBannerHeading1">Gallery</h1>
+          <h1 className="galleryBannerHeading2">
+            We wish to bring many good things to life.
+          </h1>
+        </div>
+        <div className="container">
+        <div className="row">
+            <div className="col-md-6 col-md-offset-3 text-center heading-section animate-box">
+              <h1 className="h3_gallery_heading">Our Gallery</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit
+                est facilis maiores, perspiciatis accusamus asperiores sint
+                consequuntur debitis.
+              </p>
+              <HorizontalLine className="hr_style" />
+            </div>
+          </div>
+          <div className="row row-bottom-padded-md">
+            <div className="col-md-12">
+              <ul id="fh5co-portfolio-list">
+                <MainGalleryListmap mainGalleryData={GalleryData} />
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
