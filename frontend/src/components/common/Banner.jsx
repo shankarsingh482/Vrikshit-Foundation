be71269a7b9e7133ef4b3f1e045a42aa21ff171c
Avantika Bharti <Avantika.Bharti@idc.bedbath.com>
2019-10-16 17:08:47 +0530

    banner background made generic

--- frontend/src/components/common/Banner.jsx
+++ frontend/src/components/common/Banner.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
+import './common.css';
 
-const Banner = (props) =>{
-  const {pageDescription,quotes} = props;
-  return(  <div className="galleryBanner">
-  <h1 className="galleryBannerHeading1">{pageDescription}</h1>
-  <h1 className="galleryBannerHeading2">
-   {quotes}
-  </h1>
-  </div>
-
-  )
-
-
-
-}
-export default Banner
-
+const Banner = props => {
+  const { bannerStyle, pageDescription, quotes } = props;
+  return (
+    <div className={`cover ${bannerStyle}`}>
+      <h1 className="bannerHeading1">{pageDescription}</h1>
+      <h1 className="bannerHeading2">{quotes}</h1>
+    </div>
+  );
+};
+export default Banner;
