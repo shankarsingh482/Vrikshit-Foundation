9eca5efef54b56fdb9c741a4c223a656538709a9
BA29575 <shankar.singh@idc.bedbath.com>
2019-12-10 16:58:31 +0530

    Added code for pagination

--- frontend/src/components/Pages/Team/teamContent.js
+++ frontend/src/components/Pages/Team/teamContent.js
@@ -69,4 +69,146 @@ export const teamContentData = [{
     volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
 
   },
+  {
+    id: 10,
+    image: "https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png",
+    volunteer_name: 'aaaaaaaaa',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+  },
+  {
+    id: 11,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_02.png',
+    volunteer_name: 'dsgdsgdjsdjsgdjsjgd',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 12,
+    image: "https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png",
+    volunteer_name: 'Nakul',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 13,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png',
+    volunteer_name: 'Sudarshan Maurya',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 14,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_02.png',
+    volunteer_name: 'Devika Nair',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 15,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 16,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 17,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 18,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 19,
+    image: "https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png",
+    volunteer_name: 'Aashu Verma',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+  },
+  {
+    id: 20,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_02.png',
+    volunteer_name: 'Rajnish Thakur',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 21,
+    image: "https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png",
+    volunteer_name: 'Nakul',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 22,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_01.png',
+    volunteer_name: 'Sudarshan Maurya',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 23,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_02.png',
+    volunteer_name: 'Devika Nair',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 24,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 25,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 26,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
+  {
+    id: 27,
+    image: 'https://sunlimetech.com/portfolio/boot4menu/assets/imgs/team/img_03.png',
+    volunteer_name: 'Kaushal Khatri',
+    volunteer_description: 'This is basic card with image on top, title, description and button.',
+    volunteer_summary: 'This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button. This is basic card with image on top, title, description and button.This is basic card with image on top, title, description and button.'
+
+  },
 ]
