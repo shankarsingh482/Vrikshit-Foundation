3ada3e86214f0cd673c38c0fc64cf31ce80feb36
BA29575 <shankar.singh@idc.bedbath.com>
2020-03-15 00:57:34 +0530

    formated the code

--- frontend/src/components/Pages/Team/Paginationmain.jsx
+++ frontend/src/components/Pages/Team/Paginationmain.jsx
@@ -1,34 +1,34 @@
 import React from 'react';
 import Pagination from './Pagination';
-import {teamContentData} from './teamContent';
+import { teamContentData } from './teamContent';
 
 class PaginationMain extends React.Component {
   constructor() {
-      super();
+    super();
 
-      // an example array of items to be paged
-      var exampleItems = teamContentData;
+    // an example array of items to be paged
+    var exampleItems = teamContentData;
 
-      this.state = {
-          exampleItems: exampleItems,
-          pageOfItems: exampleItems.slice(0,10),
-      };
+    this.state = {
+      exampleItems: exampleItems,
+      pageOfItems: exampleItems.slice(0, 10)
+    };
 
-      // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
-      this.onChangePage = this.onChangePage.bind(this);
+    // bind function in constructor instead of render (https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md)
+    this.onChangePage = this.onChangePage.bind(this);
   }
 
   onChangePage(pageNum) {
-      // update state with new page of items
-      this.setState({ pageOfItems: pageNum});
+    // update state with new page of items
+    this.setState({ pageOfItems: pageNum });
   }
   componentDidMount() {
     window.scrollTo(0, 0);
   }
   render() {
-      return (
-          <div>
-          <section id="team" className="pb-5">
+    return (
+      <div>
+        <section id="team" className="pb-5">
           <div className="container">
             <h5 className="section-title h1">OUR TEAM</h5>
             <div className="row">
@@ -46,15 +46,21 @@ class PaginationMain extends React.Component {
                                 alt="card image"
                               />
                             </p>
-                            <h4 className="card-title">{data.volunteer_name}</h4>
-                            <p className="card-text">{data.volunteer_summary}</p>
+                            <h4 className="card-title">
+                              {data.volunteer_name}
+                            </h4>
+                            <p className="card-text">
+                              {data.volunteer_summary}
+                            </p>
                           </div>
                         </div>
                       </div>
                       <div className="backside">
                         <div className="card">
                           <div className="card-body text-center mt-4">
-                            <h4 className="card-title">{data.volunteer_name}</h4>
+                            <h4 className="card-title">
+                              {data.volunteer_name}
+                            </h4>
                             <p className="card-text">
                               {data.volunteer_description}
                             </p>
@@ -106,9 +112,12 @@ class PaginationMain extends React.Component {
             </div>
           </div>
         </section>
-        <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage} />
-                  </div>
-      );
+        <Pagination
+          items={this.state.exampleItems}
+          onChangePage={this.onChangePage}
+        />
+      </div>
+    );
   }
 }
 
