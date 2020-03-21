29b436d402250fbd18ea791b23350f970382b13d
Avantika Bharti <Avantika.Bharti@idc.bedbath.com>
2020-03-21 14:31:52 +0530

    changes in breadcrumb

--- frontend/src/components/common/BreadCrumb.jsx
+++ frontend/src/components/common/BreadCrumb.jsx
@@ -6,24 +6,19 @@ const Breadcrumb = ({ second, third, secondRoute }) => {
   return (
     <div className="bread-bar">
       <div className="container">
-        <div className="row">
-          <div className="col-md-8 col-sm-6 col-xs-8">
-            <ol className="breadcrumb">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              {third ? (
-                <li>
-                  <Link to={`/${secondRoute}`}>{second}</Link>
-                </li>
-              ) : (
-                <li className="active">{second}</li>
-              )}
-              {third && <li className="active">{third}</li>}
-            </ol>
-          </div>
-          <div className="col-md-4 col-sm-6 col-xs-4"></div>
-        </div>
+        <ol className="breadcrumb">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {third ? (
+            <li>
+              <Link to={`/${secondRoute}`}>{second}</Link>
+            </li>
+          ) : (
+            <li className="active">{second}</li>
+          )}
+          {third && <li className="active">{third}</li>}
+        </ol>
       </div>
     </div>
   );
